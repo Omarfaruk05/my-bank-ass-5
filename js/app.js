bfc89d12657065b30de92abb6336c8aeda84c6dd
Omarfaruk05 <mdomarfrauk149518.com>
2022-02-17 18:38:10 +0600

    done

--- js/app.js
+++ js/app.js
@@ -1,50 +1,59 @@
+//function for getting input value in number 
 function getInputvalue(inputId){
     const input = document.getElementById(inputId);
     const inputText = input.value;
     const inputValue = parseFloat(inputText);
     return inputValue;
 };
+
+// function for getting text type elements inner text in number 
 function getInnerTextNumber(id){
     const innerHtml = document.getElementById(id);
     const innerText = innerHtml.innerText;
     const innerTextNumber = parseFloat(innerText);
     return innerTextNumber; 
-}
+};
+
+// function for display none error message 
 function forDidplayNone(id){
     const element = document.getElementById(id);
     element.style.display = 'none';
-}
+};
+
+// function for display block error message 
 function forDidplayBlock(id){
     const element = document.getElementById(id);
     element.style.display = 'block';
-}
+};
 
 // handling calculate button click handler
 document.getElementById('calculate-btn').addEventListener('click', function(){
     const foodCostNumber = getInputvalue('food-cost');
-
     const rentCostNumber = getInputvalue('rent-cost');
-
     const clothsCostNumber = getInputvalue('cloths-cost');
      
      //for total expenses
+    //  condition for string 
     if(isNaN(foodCostNumber) == false && isNaN(rentCostNumber) == false && isNaN(clothsCostNumber) == false){
-        if(foodCostNumber >= 0 && rentCostNumber >= 0 &&        clothsCostNumber >= 0){
+
+        // condition for minuse value 
+        if(foodCostNumber >= 0 && rentCostNumber >= 0 && clothsCostNumber >= 0){
             let totalIncomeNumber = getInputvalue('total-income');
+
+            // total expenese summation 
             var totalCostNumber = foodCostNumber + rentCostNumber + clothsCostNumber;
+
+            // condition for bigger income number 
             if(totalCostNumber < totalIncomeNumber){
                 let totalExpenses = document.getElementById('total-expenses');
                 let totalExpensesNumber = getInnerTextNumber('total-expenses');
                  totalExpensesNumber = totalCostNumber;
-                totalExpenses.innerText = totalExpensesNumber;
-    
-                let balanceNumber = getInnerTextNumber('balance');
-    
+                totalExpenses.innerText = totalExpensesNumber;   
+                let balanceNumber = getInnerTextNumber('balance');    
                 balanceNumber = totalIncomeNumber - totalExpensesNumber;
                 balance.innerText = balanceNumber;
 
- 
-                forDidplayNone('bigger-expenses')
+                forDidplayNone('bigger-expenses');
             }
             else{
 
@@ -72,12 +81,15 @@ document.getElementById('save-btn').addEventListener('click', function(){
     let remainingBalance = getInnerTextNumber('remaining-balance');
     let balanceNumber = getInnerTextNumber('balance');
 
+    // condition minus value 
     if(percentageNumber >= 0){
+
+        // for saving calculation 
         let totalSaving = (percentageNumber * totalIncomeNumber) / 100;
         if(balanceNumber > totalSaving){           
             const savingAmount = document.getElementById('saving-amount');
             savingAmount.innerText = totalSaving;
-           
+
             //remaining balance calculate
             remainingBalance = balanceNumber - totalSaving;
         
